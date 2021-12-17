ba9b9c4b18f109fa93fedb0baded685840e6b5df
yigitkemal <40325505+yigitkemal@users.noreply.github.com>
2021-12-17 10:05:37 +0300

    Long click positioning is enabled. New activity added.

--- app/src/main/java/com/example/mytraveldiary/MapsActivity.kt
+++ app/src/main/java/com/example/mytraveldiary/MapsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mytraveldiary
 
 import android.Manifest
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
@@ -21,9 +22,10 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.mytraveldiary.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.model.Marker
 import com.google.android.material.snackbar.Snackbar
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
@@ -31,6 +33,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var locationManager: LocationManager
     private lateinit var locationListener: LocationListener
     private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    private lateinit var sharedPreferences: SharedPreferences
+    private var trackBoolean: Boolean? = null
+    private var selectedLatitute: Double? = null
+    private var selectedLongitute: Double? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,6 +51,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         registerLauncher()
 
+        sharedPreferences = this.getSharedPreferences("com.example.mytraveldiary", MODE_PRIVATE)
+        trackBoolean = false
+        selectedLatitute =0.0
+        selectedLongitute = 0.0
+
     }
 
 
@@ -52,12 +63,19 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     //39.5678031, 26.802981
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        mMap.setOnMapLongClickListener(this)
+
 
         locationManager =this.getSystemService(LOCATION_SERVICE) as LocationManager
 
         locationListener = object : LocationListener {
             override fun onLocationChanged(p0: Location) {
-
+               trackBoolean = sharedPreferences.getBoolean("trackBoolean",false)
+                if(trackBoolean == false){
+                    val userLocation = LatLng(p0.latitude,p0.longitude)
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,15f))
+                    sharedPreferences.edit().putBoolean("trackBoolean",true).apply()
+                }
             }
         }
 
@@ -75,6 +93,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }else{
             // permission granted
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,10000,100f,locationListener)
+            val lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            if(lastLocation != null){
+                val lastUserLocation = LatLng(lastLocation.latitude,lastLocation.longitude)
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation,15f))
+            }
+            mMap.isMyLocationEnabled = true
         }
 
 
@@ -89,12 +113,26 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
                     // permission granted
                     locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,10000,100f,locationListener)
+                    val lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                    if(lastLocation != null){
+                        val lastUserLocation = LatLng(lastLocation.latitude,lastLocation.longitude)
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation,15f))
+                    }
+                    mMap.isMyLocationEnabled = true
                 }
             }else{
                 // permission denied
                 Toast.makeText(this,"Permission needed!",Toast.LENGTH_LONG).show()
             }
         }
+    }
+
+    override fun onMapLongClick(p0: LatLng?) {
+        mMap.clear()
+        mMap.addMarker(MarkerOptions().position(p0!!))
+
+        selectedLatitute = p0.latitude
+        selectedLongitute = p0.longitude
     }
 
 }
